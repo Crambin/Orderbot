2eae70b5a14819ab443642d69910a1eab1419ba9
Crambor <dariuscpt@gmail.com>
2020-04-25 20:42:22 +0100

    Add music playing functionality

--- constants.py
+++ constants.py
@@ -6,3 +6,18 @@ bot_developer_ids = (212241077695021056,)  # Crambor
 
 # misc
 default_prefix = '!'
+
+# music
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': 'downloads/%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0'  # ipv6 addresses cause issues sometimes
+}
