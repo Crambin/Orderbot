f3f52b392a81be9cf7a15b4d5f5b6fb3f7a7c4b1
Crambor <dariuscpt@gmail.com>
2020-12-04 16:43:16 +0000

    Replace chatbot identifier to channel id
    This allows for multiple people to talk at once, rather than having individual conversations.

--- cogs/fun.py
+++ cogs/fun.py
@@ -11,26 +11,6 @@ class Fun(commands.Cog):
         self.chat_bot = ac.Cleverbot(api_key, context=ac.DictContext()) if api_key else None
         self.is_talking = {}
 
-    @commands.command()
-    async def speak(self, ctx, *, query=None):
-        if not self.chat_bot:
-            return await ctx.send("This command is currently disabled.")
-        elif query is None:
-            return await ctx.send("You can't talk without saying anything!")
-
-        resp = await self.chat_bot.ask(query, ctx.author.id)
-        await ctx.send(resp.text)
-
-    @commands.command(aliases=('talk', 'toggle'))
-    async def toggle_talk(self, ctx):
-        self.is_talking[ctx.author.id] = self.is_talking.get(ctx.author.id, set())
-        if ctx.channel.id in self.is_talking[ctx.author.id]:
-            self.is_talking[ctx.author.id].remove(ctx.channel.id)
-            await ctx.send("I will no longer respond to you in this channel.")
-        else:
-            self.is_talking[ctx.author.id].add(ctx.channel.id)
-            await ctx.send("I will now respond to you in this channel.")
-
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.author.bot:
@@ -38,8 +18,37 @@ class Fun(commands.Cog):
         elif message.channel.id not in self.is_talking.get(message.author.id, set()):
             return
         if 3 <= len(message.content) <= 60:
-            resp = await self.chat_bot.ask(message.content, message.author.id)
+            resp = await self.chat_bot.ask(message.content, message.channel.id)
             await message.channel.send(resp.text)
+
+    @commands.command()
+    async def speak(self, ctx, *, query=None):
+        """
+        Speak to the chat bot, like Cleverbot.
+        """
+        if not self.chat_bot:
+            return await ctx.send("This command is currently disabled.")
+        elif query is None:
+            return await ctx.send("You can't talk without saying anything!")
+
+        resp = await self.chat_bot.ask(query, ctx.channel.id)
+        await ctx.send(resp.text)
+
+    @commands.command(aliases=('talk', 'toggle'))
+    async def toggle_talk(self, ctx):
+        """
+        Toggle whether the bot will respond to your messages.
+
+        This command is per-user, per-channel, meaning if you use it
+        in one channel it will only affect you in that channel, and nobody else.
+        """
+        self.is_talking[ctx.author.id] = self.is_talking.get(ctx.author.id, set())
+        if ctx.channel.id in self.is_talking[ctx.author.id]:
+            self.is_talking[ctx.author.id].remove(ctx.channel.id)
+            await ctx.send("I will no longer respond to you in this channel.")
+        else:
+            self.is_talking[ctx.author.id].add(ctx.channel.id)
+            await ctx.send("I will now respond to you in this channel.")
 
 
 def setup(bot):
